056087da5cb386242aa6bc9da53d9201b7d353ec
leejunghyun <joa3406@gmail.com>
2023-04-17 23:37:39 +0900

    good

---
+++ go.py
@@ -0,0 +1,27 @@
+#한줄에 두가지 정수 인풋 받는법
+a, b= map(int,input().split())
+
+#사칙연산 1000번:A+B
+A, B = map(int,input().split())
+print(A+B)
+print(A-B)
+print(A*B)
+print(int(A/B))
+print(A%B)
+
+#더하기 
+A, B = map(int,input().split())
+print(A+B)
+
+
+#뺄셈
+A, B = map(int,input().split())
+print(A-B)
+
+#곱셈
+A, B = map(int,input().split())
+print(A*B)
+
+#나눗셈
+A, B = map(int,input().split())
+print(A/B)

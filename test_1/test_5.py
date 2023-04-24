29394ecbc688596c39e190cabeda72c6d852b88f
leejunghyun <joa3406@gmail.com>
2023-04-24 23:24:32 +0900

    숙제완료_11021번_10952번

--- test_1/test_5.py
+++ test_1/test_5.py
@@ -1,7 +1,5 @@
 import random
-
-
-
+# 업다운 
 answer = random.randint(10, 100)
 while True:
     user_input = int(input())
@@ -15,7 +13,9 @@ while True:
 print("게임이 끝났습니다.")
 
   
- 
+
+  
+ # 11021번
 T = int(input())
 
 for i in range(1, T+1):
@@ -26,12 +26,7 @@ for i in range(1, T+1):
 
 
 
-
-
-
-# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
-
-
+# 10952번
 while True:  
     a, b = map(int, input().split())  
     if a == 0 and b == 0:  
